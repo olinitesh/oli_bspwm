9a73bf009b0e2b04e0bb968b00230d94a4aaa83d
olinitesh <49599036+olinitesh@users.noreply.github.com>
2021-12-13 13:05:13 -0500

    update pub-ip

--- etc/skel/.config/polybar/forest/scripts/pub-ip.sh
+++ etc/skel/.config/polybar/forest/scripts/pub-ip.sh
@@ -5,8 +5,17 @@
 
 IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 
+connection_list=$(nmcli -t -f name connection show --order name --active 2>/dev/null | grep "nordvpn")
+
+
 if pgrep -x openvpn > /dev/null; then
-    echo VPN: $IP
+    printf "VPN: $IP - "
+    #printf " - "
+    echo $connection_list | awk -F'-' '{print $2}'
+    
 else
-    echo $IP
+    echo Local-IP: $IP
+    #echo "# $(curl -4 -sf ifconfig.co)"
+    #ip_address=$(curl --silent https://ipaddr.pub)
+    #echo $ip_address
 fi
