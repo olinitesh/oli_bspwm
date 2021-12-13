c629942ac1d71b88eb56928c2b4b3bb776de3e3c
olinitesh <49599036+olinitesh@users.noreply.github.com>
2021-12-13 15:20:10 -0500

    Update dmenu_mine.sh

--- etc/skel/.config/rofi/dmenu_mine.sh
+++ etc/skel/.config/rofi/dmenu_mine.sh
@@ -6,5 +6,5 @@
 #rofi -show-icons -modi window, drun -show drun -theme /usr/share/rofi/themes/material.rasi
 #rofi -show-icons -modi window,drun -show drun -theme /usr/share/rofi/themes/purple.rasi
 
-rofi -show-icons -modidrun -show drun -theme ~/.config/polybar/forest/scripts/rofi/launcher.rasi
+rofi -show-icons -modidrun -show drun -theme /usr/share/rofi/themes/arthur.rasi
     #exec $terminal sh -c "$cmd && echo Press Enter to kill me... && read line"
